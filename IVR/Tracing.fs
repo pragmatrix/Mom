4d85f1ea67b835b32560b4c2bae301de6b514ae9
Armin Sander <armin@replicator.org>
2015-09-26 20:39:35 +0200

    first ideas about how tracing should work

---
+++ IVR/Tracing.fs
@@ -0,0 +1,31 @@
+﻿namespace IVR
+
+module Tracing = 
+
+    type private Tracer(instance) = 
+        member this.traceEvent e =
+            ()
+        member this.traceResult r =
+            ()
+
+    let private beginTrace instance = Tracer(instance)
+
+    /// For an IVR to be eligable for tracing, it must be declared. The instance given is to 
+    /// associate the IVR, it can be a string or any other instance, like a union cases.
+
+    let declare instance ivr = 
+
+        fun () ->
+            let tracer = beginTrace instance
+
+            let rec next ivr = 
+                match ivr with
+                | Active _ -> Active (fun e -> tracer.traceEvent e; (ivr |> IVR.step e |> next))
+                | Completed r ->
+                    tracer.traceResult r
+                    ivr
+
+            ivr |> IVR.start |> next
+        |> Delay
+
+
