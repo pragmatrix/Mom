87eba2a194299723b6b959aa8fe6b87c69177ec3
Armin Sander <armin@replicator.org>
2015-12-16 21:24:14 +0100

    split runtime in services, and support building runtimes

--- IVR/Runtime.fs
+++ IVR/Runtime.fs
@@ -16,12 +16,11 @@ module Runtime =
 
     type private CancelIVR = CancelIVR
 
-    type Runtime(host: Host) = 
+    type Runtime (host: Runtime -> Host) as this = 
+
+        let host = host this
 
         let eventQueue = SynchronizedQueue<Event>();
-
-        let delayIdGenerator = Ids.newGenerator()
-        let asyncIdGenerator = Ids.newGenerator()
 
         interface IDisposable with
             member this.Dispose() = this.cancel()
@@ -34,8 +33,6 @@ module Runtime =
             this.scheduleEvent CancelIVR
 
         member this.run ivr = 
-
-            let host = this.executeCommand
 
             let rec runLoop ivr = 
                 let event = eventQueue.dequeue()
@@ -54,25 +51,86 @@ module Runtime =
             ivr
             |> start host
             |> next
+
+    //
+    // A builder that supports the creation of runtimes and adding services to it.
+    //
+
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module Runtime =
         
-        member this.executeCommand cmd = 
+        type Service = Runtime -> Command -> Response option
+        
+        [<NoComparison;NoEquality>]
+        type Builder = {
+                services: Service list
+            }
+
+        let builder = { services = [] }
+
+        let service (service: Service) builder = 
+            { builder with
+                services = service :: builder.services }
+
+        let create builder = 
+
+            let rec serviceHost (services: Service list) runtime command = 
+                match services with
+                | [] -> () |> box
+                | s::todo ->
+                match s runtime command with
+                | Some response -> response
+                | None -> serviceHost todo runtime command
+                    
+            let services = builder.services |> List.rev
+            new Runtime (serviceHost services)
+
+    //
+    // Some predefined services that should be supported by every runtime.
+    //
+
+    module Service = 
+
+        let schedule (runtime: Runtime) (cmd: Command) = 
             match cmd with
-            | :? Schedule as s -> s.event |> this.scheduleEvent; () |> box
-            | :? Delay as d -> this.delay d
-            | :? IAsyncComputation as ac -> this.asyncComputation ac
-            | _ -> cmd |> host
+            | :? Schedule as s -> s.event |> runtime.scheduleEvent; () |> box |> Some
+            | _ -> None
 
-        member this.delay(Delay timespan) = 
-            let id = delayIdGenerator.generateId()
-            let callback _ = this.scheduleEvent (IVR.DelayCompleted id)
-            new Timer(callback, null, int64 timespan.TotalMilliseconds, -1L) |> Operators.ignore
-            id |> box
-            
-        member this.asyncComputation(ac: IAsyncComputation) = 
-            let id = asyncIdGenerator.generateId()
-            ac.run(fun r ->
-                this.scheduleEvent (IVR.AsyncComputationCompleted(id, r))
-                )
-            id |> box
+        let delay (runtime: Runtime) =
+            let delayIdGenerator = Ids.newGenerator()
+            fun (cmd : Command) ->
+                match cmd with
+                | :? Delay as d -> 
+                    let (Delay timespan) = d
+                    let id = delayIdGenerator.generateId()
+                    let callback _ = runtime.scheduleEvent (IVR.DelayCompleted id)
+                    new Timer(callback, null, int64 timespan.TotalMilliseconds, -1L) |> Operators.ignore
+                    id |> box |> Some
+                | _ -> None
 
-    let newRuntime host = new Runtime(host)
+        let async (runtime: Runtime) =
+            let asyncIdGenerator = Ids.newGenerator()
+            fun (cmd: Command) ->
+                match cmd with
+                | :? IAsyncComputation as ac -> 
+                    let id = asyncIdGenerator.generateId()
+                    ac.run(fun r ->
+                        runtime.scheduleEvent (IVR.AsyncComputationCompleted(id, r))
+                        )
+                    id |> box |> Some
+                | _ -> None
+
+    /// Runtimes a default builder, that includes the services schedule, delay, and async.
+    let defaultBuilder = 
+        Runtime.builder
+        |> Runtime.service Service.schedule
+        |> Runtime.service Service.delay
+        |> Runtime.service Service.async
+
+    /// Builds a default runtime that forwards all commands to the host. The runtime includes the
+    /// services schedule, delay, and async.
+    let newRuntime host = 
+        defaultBuilder
+        |> Runtime.service (fun _ c -> host c |> Some)
+        |> Runtime.create
+    
