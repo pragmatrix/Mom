3b6951d33b2ef41d551361e38f498ff1310281bc
Armin Sander <armin@replicator.org>
2017-02-16 18:19:31 +0100

    test if it's possible to retrieve a generic method info via a quotation expression, #17

--- IVR.Tests/SystemTests.fs
+++ IVR.Tests/SystemTests.fs
@@ -2,6 +2,7 @@
 
 open FsUnit
 open Xunit
+open Microsoft.FSharp.Quotations
 
 [<Fact>]
 let boxedValueOfUnitIsNull() = 
@@ -9,3 +10,18 @@ let boxedValueOfUnitIsNull() =
     |> should equal null
 
 
+let someGenericFunction<'t>() = ()
+
+[<Fact>]
+let canGetMethodInfoFromGenericModuleFunctionViaQuotations() = 
+        
+    let exp = <@@ someGenericFunction<obj>() @@>
+    let mi = 
+        match exp with
+        | Patterns.Call(_, mi, _) -> mi
+        | _ -> failwith "internal error"
+
+    mi.Name |> should equal "someGenericFunction"
+    mi.IsGenericMethod |> should equal true
+    mi.IsGenericMethodDefinition |> should equal false
+    mi.GetGenericMethodDefinition().IsGenericMethodDefinition |> should equal true
