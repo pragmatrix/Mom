f449247753812346eb5b62f0b11a37f59eececae
Armin Sander <armin@replicator.org>
2025-02-24 11:39:10 +0100

    Update all dependencies

--- Mom.Tests/ReflectorTests.fs
+++ Mom.Tests/ReflectorTests.fs
@@ -21,3 +21,20 @@ let ``Reflector sends and receive a message``() =
     |> Mom.start
     |> Flux.isCompleted
     |> should be True
+
+[<Fact>]
+let ``Reflector sends and two parallel receiver receive it`` () =
+    
+    let wrapped = Reflector.wrap ^ fun (sender, receiver) ->  mom {
+        // send a message
+        do! sender(())
+        // then receive it.
+        let! _ = Mom.race [receiver(); receiver()]
+        ()
+    }
+
+    // Expected to complete in one go.
+    wrapped
+    |> Mom.start
+    |> Flux.isCompleted
+    |> should be True
