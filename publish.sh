d6219dc54935bad7f7fdfda55d52d613fa814758
Armin Sander <armin@replicator.org>
2025-02-24 16:01:39 +0100

    Fix FSharp.Core dependency, and move to paket pack

--- publish.sh
+++ publish.sh
@@ -2,5 +2,10 @@ set -ex
 dotnet tool restore
 dotnet clean -c Release
 rm -f Mom/bin/Release/*.nupkg
-dotnet pack -c Release Mom
+
+# 20250224 Moved to dotnet paket pack after attempting to require only FSharp.Core >= 6.0.3 and trying to add an appropriate nuspec file, which screwed things up completely.
+dotnet build -c Release Mom
+dotnet paket pack Mom/bin/Release
+# dotnet pack -c Release Mom
+
 dotnet nuget push Mom/bin/Release/Mom.*.nupkg --source https://www.myget.org/F/pragmatrix --api-key $MYGETAPIKEY
